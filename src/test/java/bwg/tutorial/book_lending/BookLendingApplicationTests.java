b5aef0a50ab17be79b5ed9ef60299079bdca82a3
Chris Hamatake <chamatake@gmail.com>
2025-08-07 16:33:23 -0600

    Initial commit with basics

---
+++ src/test/java/bwg/tutorial/book_lending/BookLendingApplicationTests.java
@@ -0,0 +1,13 @@
+package bwg.tutorial.book_lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
